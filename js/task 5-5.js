e232e52ffff07ba4764b0cb34f8a9cb5d6559a65
Ihor Prokopets <kiringa23@gmail.com>
2020-10-27 09:44:07 +0200

    add initial files

---
+++ js/task 5-5.js
@@ -0,0 +1,69 @@
+/* Задача 5-5 класс Напиши класс Car с указанными свойствами и методами.
+ 
+ */
+
+class Car {
+  static getSpecs(car) {
+    return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`;
+  }
+  constructor({ maxSpeed, price }) {
+    this.maxSpeed = maxSpeed;
+    this.speed = 0;
+    this._price = price;
+    this.isOn = false;
+    this.distance = 0;
+  }
+  get price() {
+    return this._price;
+  }
+  set price(value) {
+    return (this._price = value);
+  }
+  turnOn() {
+    this.isOn = true;
+  }
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  accelerate(value) {
+    if (value < this.maxSpeed) {
+      this.speed += value;
+    } else {
+      this.speed = this.maxSpeed;
+    }
+  }
+  decelerate(value) {
+    if (this.speed > value) {
+      this.speed -= value;
+    } else {
+      this.speed = 0;
+    }
+  }
+  drive(hours) {
+    if (this.isOn === true) {
+      this.distance += hours * this.speed;
+    }
+  }
+}
+
+console.dir(Car);
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
